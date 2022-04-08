4316f47b6ac710be01cbfc6bc965e75aad264075
Kbza <g.leonel.m17@gmail.com>
2022-04-08 16:29:02 -0300

    Botones Javascript

---
+++ js/Stock.js
@@ -0,0 +1,16 @@
+let stock = [ 
+    {Ingrediente: "Harina", Cantidad: 0}, {Ingrediente: "Huevos", Cantidad: 0}, {Ingrediente: "Sal", Cantidad: 0}, 
+    {Ingrediente: "Aceite", Cantidad: 0}, {Ingrediente: "Vinagre", Cantidad: 0}, 
+    {Rellenos: [{Ingrediente: "Ricota", Cantidad: 0}, {Ingrediente: "Jamon", Cantidad: 0}, {Ingrediente: "Muzzarela",Cantidad: 0}, {Ingrediente: "Cebolla", Cantidad: 0}, 
+    {Ingrediente: "Calabaza", Cantidad: 0}, {Ingrediente: "Choclo", Cantidad: 0}, {Ingrediente: "Nuez", Cantidad: 0}, {Ingrediente: "Tomate", Cantidad: 0}, {Ingrediente: "Albahaca", Cantidad: 0},
+    {Ingrediente: "Roquefort", Cantidad: 0}, {Ingrediente: "Pollo", Cantidad: 0}, {Ingrediente: "Espinaca", Cantidad: 0}]}
+    ]
+
+
+function agregar() {
+    alert("Agregado con exito")
+}
+
+function agregarharina() {
+    document.getElementById("Harina").innerText = 1
+}
