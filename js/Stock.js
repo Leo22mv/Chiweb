10a0f5da87caaeb4cc366513f72c6f0cd5687478
Kbza <g.leonel.m17@gmail.com>
2022-04-11 13:24:53 -0300

    Metiendo Javascript y CSS

--- js/Stock.js
+++ js/Stock.js
@@ -7,10 +7,42 @@ let stock = [
     ]
 
 
+
+
 function agregar() {
+    const form = document.getElementById("formAgregar")
+
+    let transactionFormData = new FormData(form)
+
+    stock[0].Cantidad += transactionFormData.get("Cantidad1")
+    stock[1].Cantidad += transactionFormData.get("Cantidad2")
+    stock[5].Rellenos[1].Cantidad += transactionFormData.get("Cantidad3")
+    stock[5].Rellenos[2].Cantidad += transactionFormData.get("Cantidad4")
+    stock[5].Rellenos[0].Cantidad += transactionFormData.get("Cantidad5")
+    stock[5].Rellenos[6].Cantidad += transactionFormData.get("Cantidad6")
+    stock[5].Rellenos[11].Cantidad += transactionFormData.get("Cantidad7")
+    stock[5].Rellenos[4].Cantidad += transactionFormData.get("Cantidad8")
+    stock[5].Rellenos[9].Cantidad += transactionFormData.get("Cantidad9")
+    stock[5].Rellenos[7].Cantidad += transactionFormData.get("Cantidad10")
+    stock[5].Rellenos[8].Cantidad += transactionFormData.get("Cantidad11")
+    stock[5].Rellenos[10].Cantidad += transactionFormData.get("Cantidad12")
+    stock[5].Rellenos[5].Cantidad += transactionFormData.get("Cantidad13")
+
     alert("Agregado con exito")
 }
 
-function agregarharina() {
-    document.getElementById("Harina").innerText = 1
+function actualizar() {
+    document.getElementById("Harina").innerText = stock[0].Cantidad
+    document.getElementById("Huevos").innerText = stock[1].Cantidad
+    document.getElementById("Jamon").innerText = stock[5].Rellenos[1].Cantidad
+    document.getElementById("Queso").innerText = stock[5].Rellenos[2].Cantidad
+    document.getElementById("Ricota").innerText = stock[5].Rellenos[0].Cantidad
+    document.getElementById("Nuez").innerText = stock[5].Rellenos[6].Cantidad
+    document.getElementById("Espinaca").innerText = stock[5].Rellenos[11].Cantidad
+    document.getElementById("Calabaza").innerText = stock[5].Rellenos[4].Cantidad
+    document.getElementById("Roquefort").innerText = stock[5].Rellenos[9].Cantidad
+    document.getElementById("Tomate").innerText = stock[5].Rellenos[7].Cantidad
+    document.getElementById("Albahaca").innerText = stock[5].Rellenos[8].Cantidad
+    document.getElementById("Pollo").innerText = stock[5].Rellenos[10].Cantidad
+    document.getElementById("Choclo").innerText = stock[5].Rellenos[5].Cantidad
 }
